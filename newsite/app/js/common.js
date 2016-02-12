da062dcdf500b1af9c9342d2379a083c80ec3f77
moroz <morozarturv@gmail.com>
2016-02-12 18:04:03 +0700

    main desktop

--- newsite/app/js/common.js
+++ newsite/app/js/common.js
@@ -10,4 +10,38 @@ head.ready(function() {
 		};
 	}());
 
+	// premiers
+	(function () {
+		var sl = $('.js-blog');
+		if (sl.length) {
+			sl.slick({
+				slidesToShow: 5
+			});
+		};
+	}());
+
+	// premiers
+	(function () {
+		var sl = $('.js-premiers');
+		if (sl.length) {
+			sl.slick({
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				dots: true
+			});
+		};
+	}());
+
+	// slider
+	(function () {
+		var sl = $('.js-slider');
+		if (sl.length) {
+			sl.slick({
+				slidesToShow: 6,
+				slidesToScroll: 6,
+				dots: true
+			});
+		};
+	}());
+
 });
