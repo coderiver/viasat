fed8e48a70959fd4f85ede0457f2b89e2e79e643
hraboviyvadim <hraboviyvadim@gmail.com>
2016-04-26 13:38:25 +0300

    code refactoring, template upd, home page upd

--- newsite/app/js/init.js
+++
@@ -1,3 +0,0 @@
-head.load("js/lib/jquery.js", 
-			"js/lib/slick.min.js",
-			"js/common.js");
---
+++ newsite/src/js/common.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+    // menu
+    (function () {
+        var btn  = $('.js-menu-btn'),
+            menu = $('.js-menu');
+        btn.on('click', function () {
+            btn.toggleClass('is-active');
+            menu.toggleClass('is-active');
+        });
+    }());
+
+    // promo
+    (function () {
+        var sl = $('.js-promo');
+        if (sl.length) {
+            sl.slick({
+                dots: true
+            });
+        };
+    }());
+
+    // premiers
+    (function () {
+        var sl = $('.js-blog');
+        if (sl.length) {
+            sl.slick({
+                slidesToShow: 5,
+                responsive: [{
+                    breakpoint: 1023,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        dots: true
+                    }
+                },{
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true
+                    }
+                }]
+            });
+        };
+    }());
+
+    // default slider
+    (function () {
+        var sl = $('.js-slider');
+        if (sl.length) {
+            sl.slick({
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                responsive: [{
+                    breakpoint: 1023,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },{
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+            });
+        };
+    }());
+
+    // channels slider
+    (function () {
+        var sl = $('.js-channels');
+        if (sl.length) {
+            sl.slick({
+                slidesToShow: 6,
+                slidesToScroll: 6,
+                dots: true,
+                responsive: [{
+                    breakpoint: 1023,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },{
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+            });
+        };
+    }());
+
+});
